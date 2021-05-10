8585f2e4561a2118e4480419b10513021ff2b4a5
nnaaa <nnaaanachi@gmail.com>
2021-05-09 23:33:31 -0400

    Initial commit

---
+++ XSOverlay VRChat Parser Updater/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XSOverlay_VRChat_Parser_Updater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
